866264a076c2004dc8c364f4094c68785bc62b74
Juan Galdino <jgaldino.cs@gmail.com>
2023-06-13 17:59:23 -0300

    Turned object into Array at DataStorage file

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,17 +1,25 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { map } from "rxjs";
+import { map, tap } from "rxjs";
 import { environment } from "src/environments/environment";
+import { ProductListService } from "../product-list/product-list.service";
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private productListService: ProductListService) {}
 
   storeProducts() {
   }
 
   fetchProducts() {
-    this.http.get(environment.API_URL)
-      .pipe(map(products => products[0]))
+    return this.http.get(environment.API_URL)
+      .pipe(
+        map(products => {
+          for(let key in products) {
+            return products[key]
+          }
+        }),
+        tap(products => this.productListService.products = products)      
+      )
   }
 }
