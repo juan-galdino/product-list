f7c21e51849ac10545b8646b646c5738734263d0
Juan Galdino <jgaldino.cs@gmail.com>
2023-06-16 15:36:07 -0300

    Added header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { AdditionFormComponent } from './addition-form/addition-form.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    AdditionFormComponent
+    AdditionFormComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
